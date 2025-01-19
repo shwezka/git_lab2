bcd9fc7b2d712f44f7859aa1216a48549e2f0883
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 20:40:40 +0300

    minus method

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -3,13 +3,16 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static int inputB(){
-        Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
+    public static int div(int a, int b){
+        if(b!=0){
+            return a/b;
+        }
+        else{
+            System.out.println("Нельзя делить на 0!");
+            return 0;
+        }
     }
     public static void main(String[] args) {
-        int b;
-        b = inputB();
-        System.out.println(b);
+        System.out.println(div(6,2));
     }
 }
