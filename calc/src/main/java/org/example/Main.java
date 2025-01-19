5aca81714630009dff8c11d11c2b5ee0761332fa
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 21:05:48 +0300

    Plus

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
+
+import static org.example.Plus.plus;
 
 public class Main {
-    public static int plus(int a, int b){
-        return a+b;
-    }
+
     public static void main(String[] args) {
         System.out.println(plus(1,3));
     }
