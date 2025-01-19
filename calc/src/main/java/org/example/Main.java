e57b78a153d1281432e59dbf6f1b79cb2380a397
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 20:10:21 +0300

    initial commit

---
+++ calc/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Выберите опцию:");
+        System.out.println("1 -> Ввести А");
+        System.out.println("2 -> Ввести В:");
+        System.out.println("3 -> выполнить операцию +");
+        System.out.println("4 -> выполнить операцию -");
+        System.out.println("5 -> выполнить операцию *");
+        System.out.println("6 -> выполнить операцию /");
+    }
+}
