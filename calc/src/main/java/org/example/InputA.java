5d0606aa827722f19ca7464b74002782f8717894
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 21:01:58 +0300

    InputA

---
+++ calc/src/main/java/org/example/InputA.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class InputA {
+    public static int inputA(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
+    }
+}
--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import java.util.Scanner;
+
+import static org.example.InputA.inputA;
 
 public class Main {
-    public static int inputA(){
-        Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
-    }
+
     public static void main(String[] args) {
-        int a = 0;
+        int a;
         a = inputA();
         System.out.println(a);
     }
