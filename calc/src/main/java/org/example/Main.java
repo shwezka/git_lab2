158c8da38bd7d9fed6b1adadfb25dd98a5239bb1
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 20:43:28 +0300

    mult method

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -3,13 +3,11 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static int inputB(){
-        Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
+    public static int mult(int a, int b){
+        return a*b;
     }
     public static void main(String[] args) {
-        int b;
-        b = inputB();
-        System.out.println(b);
+
+        System.out.println(mult(2,5));
     }
 }
