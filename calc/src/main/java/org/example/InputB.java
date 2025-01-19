9b48b54dce44d662fa3a0c3bf0e3f2f9a060de9e
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 21:10:59 +0300

    merging

---
+++ calc/src/main/java/org/example/InputB.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class InputB {
+    public static int inputB(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
+    }
+}
--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import static org.example.InputA.inputA;
+import static org.example.InputB.inputB;
 
 public class Main {
 
@@ -9,5 +10,8 @@ public class Main {
         int a;
         a = inputA();
         System.out.println(a);
+        int b;
+        b = inputB();
+        System.out.println(b);
     }
 }
