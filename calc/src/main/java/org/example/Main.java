ee8dcfa2a7b889d61b8b40d2c7b164353b3ffa05
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 21:03:39 +0300

    Div

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -1,17 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
+
+import static org.example.Div.div;
 
 public class Main {
-    public static int div(int a, int b){
-        if(b!=0){
-            return a/b;
-        }
-        else{
-            System.out.println("Нельзя делить на 0!");
-            return 0;
-        }
-    }
+
     public static void main(String[] args) {
         System.out.println(div(6,2));
     }
