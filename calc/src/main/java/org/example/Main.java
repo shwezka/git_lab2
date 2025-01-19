16618efe98dbb6b8dcc24f885dd97ab5405f5c54
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 20:59:47 +0300

    InputB

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -1,15 +1,11 @@
 package org.example;
 
-import java.util.Scanner;
+import static org.example.InputB.inputB;
 
 public class Main {
-    public static int inputB(){
-        Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
-    }
     public static void main(String[] args) {
-        int b;
-        b = inputB();
-        System.out.println(b);
+        int a;
+        a = inputB();
+        System.out.println(a);
     }
 }
