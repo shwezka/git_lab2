b145f6d42a304640f9ef0c747315c375daf3c2b8
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 21:05:09 +0300

    Mult

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
+
+import static org.example.Mult.mult;
 
 public class Main {
-    public static int mult(int a, int b){
-        return a*b;
-    }
+
     public static void main(String[] args) {
 
         System.out.println(mult(2,5));
