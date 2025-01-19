f76c4c882c36d3649767e8414df5fa291c079d7f
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 20:55:42 +0300

    input a

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -3,13 +3,13 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static int inputB(){
+    public static int inputA(){
         Scanner scan = new Scanner(System.in);
         return scan.nextInt();
     }
     public static void main(String[] args) {
-        int b;
-        b = inputB();
-        System.out.println(b);
+        int a;
+        a = inputA();
+        System.out.println(a);
     }
 }
