e6e02bc029f4aa677027f5cf01f46cbed9657308
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 20:34:17 +0300

    plus method

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -3,13 +3,10 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static int inputB(){
-        Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
+    public static int plus(int a, int b){
+        return a+b;
     }
     public static void main(String[] args) {
-        int b;
-        b = inputB();
-        System.out.println(b);
+        System.out.println(plus(1,3));
     }
 }
